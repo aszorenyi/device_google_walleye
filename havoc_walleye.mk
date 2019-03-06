ec8e7a954d93ad9d04f0e78e19c9b13b576794dd
Everett Westman <38740385+TCU14@users.noreply.github.com>
2019-03-05 21:21:00 -0700

    Update Fingerprint to March 2019 (PQ2A.190305.002)

--- havoc_walleye.mk
+++ havoc_walleye.mk
@@ -46,8 +46,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ2A.190205.002/5164942:user/release-keys
-    PRIVATE_BUILD_DESC="walleye-user 9 PQ2A.190205.002 5164942 release-keys"
+    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ2A.190305.002/5240760:user/release-keys
+    PRIVATE_BUILD_DESC="walleye-user 9 PQ2A.190305.002 5240760 release-keys"
 
     
 PRODUCT_COPY_FILES += \
