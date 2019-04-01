3b7c192b3e71b8625446fb162d7d28df3aaf537e
TCU14 <ultimatecreepermaster@gmail.com>
2019-04-01 11:39:20 -0600

    Update Fingerprint to PQ2A.190405.003, Apr 2019

--- havoc_walleye.mk
+++ havoc_walleye.mk
@@ -46,8 +46,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ2A.190305.002/5240760:user/release-keys
-    PRIVATE_BUILD_DESC="walleye-user 9 PQ2A.190305.002 5240760 release-keys"
+    BUILD_FINGERPRINT=google/walleye/walleye:9/PQ2A.190405.003/5310204:user/release-keys
+    PRIVATE_BUILD_DESC="walleye-user 9 PQ2A.190405.003 5310204 release-keys"
 
     
 PRODUCT_COPY_FILES += \
