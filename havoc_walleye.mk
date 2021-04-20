f7e8b01b98e906e7c62f851d68661f4a3845d204
Lunarixus <Nathan@lunarixus.dev>
2021-04-20 12:56:17 +0530

    walleye: Move audio configs to a separate directory

--- havoc_walleye.mk
+++ havoc_walleye.mk
@@ -44,9 +44,6 @@ $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 $(call inherit-product, device/google/walleye/device.mk)
 $(call inherit-product, vendor/google/walleye/walleye-vendor.mk)
 
-PRODUCT_COPY_FILES += \
-    device/google/walleye/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
-
 PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := google
 PRODUCT_NAME := havoc_walleye
