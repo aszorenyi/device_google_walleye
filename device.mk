caba7f8b1d06c476130433095ff4d7833eabfb68
Lunarixus <Nathan@lunarixus.dev>
2021-04-20 13:11:29 +0530

    walleye: Move NFC configs to a separate directory

--- device.mk
+++ device.mk
@@ -92,8 +92,9 @@ PRODUCT_COPY_FILES += \
 
 include device/google/wahoo/device.mk
 
+# NFC config
 PRODUCT_COPY_FILES += \
-    device/google/walleye/nfc/libnfc-nxp.muskie.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    device/google/walleye/configs/nfc/libnfc-nxp.muskie.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
     device/google/walleye/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
